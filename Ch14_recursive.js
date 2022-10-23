7dccfcfd4628ee970c88ddbc269e561c01ac203f
Hwan <dnfwlq8054@naver.com>
2022-10-23 17:39:32 +0900

    Update Ch14_recursive.js

--- Ch14_recursive.js
+++ Ch14_recursive.js
@@ -41,3 +41,8 @@ const new_obj = deepChangeObj(obj, changeObj)
 
 console.log(`obj: ${JSON.stringify(obj)}`)
 console.log(`new_obj: ${JSON.stringify(new_obj)}`)
+
+# ---- OutPut
+# obj: {"name":"hwan","number":10,"payload":{"action":"go","size":10,"user_id":14444},"resurce":[{"arn":"asjelfjlfjl","token_number":10}],"option":true}
+# new_obj: {"name":"hwan","number":10,"payload":{"action":"back","size":10,"user_id":14444},"resurce":{"0":{"arn":"hahahahahaha","token_number":10}},"option":true}
+# ----
